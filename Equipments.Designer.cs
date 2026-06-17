de78cf7ded42a96463a8cc64b09379d86f91c5e7
sachintha-studio <sachintha.kvd@gmail.com>
2026-06-17 18:20:46 +0530

    create equipment section

---
+++ Equipments.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace gym_management_system
+{
+    partial class Equipments
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnViewEq = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.dateTimePickerDeliveryDate = new System.Windows.Forms.DateTimePicker();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtMusclesUsed = new System.Windows.Forms.TextBox();
+            this.txtDescription = new System.Windows.Forms.RichTextBox();
+            this.txtCost = new System.Windows.Forms.TextBox();
+            this.txtEquipName = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnViewEq
+            // 
+            this.btnViewEq.Location = new System.Drawing.Point(404, 397);
+            this.btnViewEq.Name = "btnViewEq";
+            this.btnViewEq.Size = new System.Drawing.Size(130, 23);
+            this.btnViewEq.TabIndex = 28;
+            this.btnViewEq.Text = "View Equipments";
+            this.btnViewEq.UseVisualStyleBackColor = true;
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(292, 397);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(75, 23);
+            this.btnReset.TabIndex = 27;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(183, 397);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 26;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // dateTimePickerDeliveryDate
+            // 
+            this.dateTimePickerDeliveryDate.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePickerDeliveryDate.Location = new System.Drawing.Point(251, 266);
+            this.dateTimePickerDeliveryDate.Name = "dateTimePickerDeliveryDate";
+            this.dateTimePickerDeliveryDate.Size = new System.Drawing.Size(283, 22);
+            this.dateTimePickerDeliveryDate.TabIndex = 25;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(97, 317);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(34, 16);
+            this.label6.TabIndex = 24;
+            this.label6.Text = "Cost";
+            // 
+            // txtMusclesUsed
+            // 
+            this.txtMusclesUsed.Location = new System.Drawing.Point(251, 226);
+            this.txtMusclesUsed.Name = "txtMusclesUsed";
+            this.txtMusclesUsed.Size = new System.Drawing.Size(283, 22);
+            this.txtMusclesUsed.TabIndex = 23;
+            // 
+            // txtDescription
+            // 
+            this.txtDescription.Location = new System.Drawing.Point(251, 106);
+            this.txtDescription.Name = "txtDescription";
+            this.txtDescription.Size = new System.Drawing.Size(283, 96);
+            this.txtDescription.TabIndex = 22;
+            this.txtDescription.Text = "";
+            // 
+            // txtCost
+            // 
+            this.txtCost.Location = new System.Drawing.Point(251, 314);
+            this.txtCost.Name = "txtCost";
+            this.txtCost.Size = new System.Drawing.Size(283, 22);
+            this.txtCost.TabIndex = 21;
+            // 
+            // txtEquipName
+            // 
+            this.txtEquipName.Location = new System.Drawing.Point(251, 64);
+            this.txtEquipName.Name = "txtEquipName";
+            this.txtEquipName.Size = new System.Drawing.Size(283, 22);
+            this.txtEquipName.TabIndex = 20;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(97, 271);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(89, 16);
+            this.label5.TabIndex = 19;
+            this.label5.Text = "Delivery Date";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(133, 232);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 16);
+            this.label4.TabIndex = 18;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(97, 229);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(93, 16);
+            this.label3.TabIndex = 17;
+            this.label3.Text = "Muscles Used";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(97, 109);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 16);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "Description";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(97, 67);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(111, 16);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "Equipment Name";
+            // 
+            // Equipments
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(630, 484);
+            this.Controls.Add(this.btnViewEq);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.dateTimePickerDeliveryDate);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtMusclesUsed);
+            this.Controls.Add(this.txtDescription);
+            this.Controls.Add(this.txtCost);
+            this.Controls.Add(this.txtEquipName);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Equipments";
+            this.Text = "Equipments";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnViewEq;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.DateTimePicker dateTimePickerDeliveryDate;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtMusclesUsed;
+        private System.Windows.Forms.RichTextBox txtDescription;
+        private System.Windows.Forms.TextBox txtCost;
+        private System.Windows.Forms.TextBox txtEquipName;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ Equipments.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace gym_management_system
+{
+    public partial class Equipments : Form
+    {
+        public Equipments()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            string EquipName = txtEquipName.Text;
+            string Description = txtDescription.Text;
+            string MUsed = txtMusclesUsed.Text;
+            string DDate = dateTimePickerDeliveryDate.Text;
+            Int64 cost = Int64.Parse(txtCost.Text);
+
+            /*
+                SqlConnection con = new SqlConnection();
+                con.ConnectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;Initial Catalog=Equipments;Integrated Security=True";
+
+                string query = "INSERT INTO NewStaff (Fname, Lname, Gender, Dob, Mobile, Email, JoinDate, Statee, City) VALUES (@fname, @lname, @gender, @dob, @mobile, @email, @jdate, @state, @city)";
+
+                using (SqlCommand cmd = new SqlCommand(query, con))
+             {
+                cmd.Parameters.AddWithValue("@fname", txtFname.Text);
+                cmd.Parameters.AddWithValue("@lname", txtLname.Text);
+                cmd.Parameters.AddWithValue("@gender", txtGender.Text);
+                cmd.Parameters.AddWithValue("@dob", txtDob.Text);
+                cmd.Parameters.AddWithValue("@mobile", txtMobile.Text);
+                cmd.Parameters.AddWithValue("@email", txtEmail.Text);
+                cmd.Parameters.AddWithValue("@jdate", txtJoinDate.Text);
+                cmd.Parameters.AddWithValue("@state", txtState.Text);
+                cmd.Parameters.AddWithValue("@city", txtCity.Text);
+
+                con.Open();
+                cmd.ExecuteNonQuery();
+                con.Close();
+             }
+
+                MessageBox.Show("Data saved.", "Inserted", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+             */
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            txtEquipName.Clear();
+            txtDescription.Clear();
+            txtMusclesUsed.Clear();
+            txtCost.Clear();
+            dateTimePickerDeliveryDate.Value = DateTime.Now;
+        }
+    }
+}
